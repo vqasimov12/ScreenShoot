5bb6c0b597a72413c365e32aaf157704ca55e1f9
Qasim_wu20 <Qasim_wu20@STHQ0118-07>
2024-09-13 11:54:53 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Timers;
+
+
+var client = new UdpClient();
+var ep = new IPEndPoint(IPAddress.Loopback, 27001);
+var Timer = new System.Timers.Timer();
+var path = "Image.png";
+_ = Task.Run(() =>
+{
+    Timer.Interval = 3000;
+    Timer.Elapsed += ScreenShot;
+    Timer.AutoReset = true;
+    Timer.Enabled = true;
+    Timer.Start();
+});
+void ScreenShot(object? source, ElapsedEventArgs? e)
+{
+    using (Bitmap bitmap = new Bitmap(1920, 1080))
+    {
+        using Graphics g = Graphics.FromImage(bitmap);
+        g.CopyFromScreen(Point.Empty, Point.Empty, new Size(1920, 1080));
+        bitmap.Save(path, ImageFormat.Png);
+       
+    }
+}
+
+while (true)
+{
+    try
+    {
+        if (!File.Exists(path))
+            continue;
+        using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+        int len = 22;
+        var bytes = new byte[len];
+        var fileSize = fs.Length;
+        do
+        {
+            len = fs.Read(bytes, 0, len);
+            client.Send(bytes, ep);
+        } while (len > 0);
+        File.Delete(path);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.IO;
+
+namespace Server;
+
+public partial class MainWindow : Window
+{
+    string path = "image.png";
+    UdpClient client = new();
+    IPEndPoint ep = new(IPAddress.Any, 0);
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        _ = Task.Run(() =>
+        {
+
+            while (true)
+            {
+                try
+                {
+                    var btyes = client.Receive(ref ep);
+                   File.WriteAllBytes(path, btyes);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        });
+    }
+
+
+
+
+}
